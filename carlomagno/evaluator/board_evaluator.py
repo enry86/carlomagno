a9fb73c4b67d48b36988934df5f0da08433b7663
Enrico Sartori <sartori.enrico@gmail.com>
2024-08-13 11:50:05 +0200

    wip testing models

--- carlomagno/evaluator/board_evaluator.py
+++ carlomagno/evaluator/board_evaluator.py
@@ -48,6 +48,7 @@ class BoardEvaluator():
             board.push(m)                        
             v = cm.board_to_vector(board, next_turn)
             str_v = '|'.join(map(lambda x: str(x), v)) 
+            print(len(self.boards))
             if str_v in self.boards:
                 #print(f'Board [{str_v}] already played, skip move to avoid loops')
                 board.pop()
